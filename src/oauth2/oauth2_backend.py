d1b665ebaf4ef58e2c251fd218f6aa7b1b7e490d
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2023-12-05 14:56:58 -0500

    feat: initial commit

--- src/oauth2/oauth2_backend.py
+++ src/oauth2/oauth2_backend.py
@@ -12,19 +12,20 @@ from starlette.requests import HTTPConnection
 
 from models import AuthenticatedUser
 
-from .oauth2_admin import oauth2_admin
+from .oauth2_admin import OAuth2Admin
 
 log = logging.getLogger(__name__)
 
 
 class BearerTokenAuthBackend(AuthenticationBackend):
-    def __init__(self, token_bearer: OAuth2AuthorizationCodeBearer) -> None:
+    def __init__(self, token_bearer: OAuth2AuthorizationCodeBearer, oauth2_admin: OAuth2Admin) -> None:
         self.token_bearer = token_bearer
+        self.oauth2_admin = oauth2_admin
 
     async def authenticate(self, conn: HTTPConnection) -> Coroutine[Any, Any, Tuple[AuthCredentials, BaseUser] | None]:
         try:
             token = await self.token_bearer(conn)
-            claims = oauth2_admin.get_claims(token)
+            claims = self.oauth2_admin.get_claims(token)
             if claims is not None:
                 auths = (
                     self.extract_nested(claims, "resource_access", "realm-management", "roles")
