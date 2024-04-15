aaa7314d7514d7d485186cf212ce05030764cbce
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2024-04-15 14:20:18 -0400

    feat: add logging

--- src/regtech_api_commons/api/exception_handlers.py
+++ src/regtech_api_commons/api/exception_handlers.py
@@ -1,10 +1,13 @@
+import logging
 from starlette.responses import JSONResponse
 from starlette.requests import Request
 
 from regtech_api_commons.api.exceptions import RegTechHttpException
 
+log = logging.getLogger(__name__)
 
 async def regtech_http_exception_handler(request: Request, exception: RegTechHttpException) -> JSONResponse:
+    log.error(exception, exc_info=True, stack_info=True)
     return JSONResponse(
         status_code=exception.status_code, content={"error_name": exception.name, "error_detail": exception.detail}
     )
--- tests/api/test_exception_handlers.py
+++ tests/api/test_exception_handlers.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 import json
 from typing import Dict
+from unittest.mock import ANY
 
 from fastapi import Request
 import pytest
 from pytest_mock import MockerFixture
-from regtech_api_commons.api.exception_handlers import regtech_http_exception_handler
+from regtech_api_commons.api.exception_handlers import regtech_http_exception_handler, log as exception_logger
 from regtech_api_commons.api.exceptions import RegTechHttpException
 
 
@@ -15,7 +16,8 @@ def mock_request(mocker: MockerFixture) -> Request:
     return mock.return_value
 
 
-async def test_regtech_http_exception_handler(mock_request: Request):
+async def test_regtech_http_exception_handler(mocker: MockerFixture, mock_request: Request):
+    error_log_spy = mocker.patch.object(exception_logger, "error")
     e = RegTechHttpException(HTTPStatus.INTERNAL_SERVER_ERROR, name="Test Exception", detail="test exception detail")
     response = await regtech_http_exception_handler(mock_request, e)
     assert response.status_code == e.status_code
@@ -23,6 +25,7 @@ async def test_regtech_http_exception_handler(mock_request: Request):
     assert content["error_name"] == e.name
     assert content["error_detail"] == e.detail
     assert isinstance(content["error_detail"], str)
+    error_log_spy.assert_called_with(e, exc_info=True, stack_info=True)
 
 
 async def test_regtech_http_exception_handler_nested_detail(mock_request: Request):
