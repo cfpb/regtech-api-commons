fecdeceb4ad330a8c06fab974a2b7bccdfc7b7ae
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2024-04-18 13:17:59 -0400

    feat: add handlers for all catchable exceptions

--- src/regtech_api_commons/api/exception_handlers.py
+++ src/regtech_api_commons/api/exception_handlers.py
@@ -4,17 +4,21 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
 from starlette.requests import Request
+from starlette.exceptions import HTTPException
 
 from regtech_api_commons.api.exceptions import RegTechHttpException
 
 log = logging.getLogger(__name__)
+
+ERROR_NAME = "error_name"
+ERROR_DETAIL = "error_detail"
 
 
 async def regtech_http_exception_handler(request: Request, exception: RegTechHttpException) -> JSONResponse:
     log.error(exception, exc_info=True, stack_info=True)
     return JSONResponse(
         status_code=exception.status_code,
-        content={"error_name": exception.name, "error_detail": jsonable_encoder(exception.detail)},
+        content={ERROR_NAME: exception.name, ERROR_DETAIL: jsonable_encoder(exception.detail)},
     )
 
 
@@ -22,5 +26,20 @@ async def request_validation_error_handler(request: Request, exception: RequestV
     log.warn(exception, exc_info=True, stack_info=True)
     return JSONResponse(
         status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-        content={"error_name": "Request Validation Failure", "error_detail": jsonable_encoder(exception.errors())},
+        content={ERROR_NAME: "Request Validation Failure", ERROR_DETAIL: jsonable_encoder(exception.errors())},
+    )
+
+async def http_exception_handler(request: Request, exception: HTTPException) -> JSONResponse:
+    log.error(exception, exc_info=True, stack_info=True)
+    status = HTTPStatus(exception.status_code)
+    return JSONResponse(
+        status_code=exception.status_code,
+        content={ERROR_NAME: status.phrase, ERROR_DETAIL: jsonable_encoder(exception.detail)},
+    )
+
+async def general_exception_handler(request: Request, exception: Exception) -> JSONResponse:
+    log.error(exception, exc_info=True, stack_info=True)
+    return JSONResponse(
+        status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+        content={ERROR_NAME: HTTPStatus.INTERNAL_SERVER_ERROR.phrase, ERROR_DETAIL: "server error"},
     )
--- tests/api/test_exception_handlers.py
+++ tests/api/test_exception_handlers.py
@@ -2,13 +2,18 @@ from http import HTTPStatus
 import json
 from typing import Dict
 
-from fastapi import Request
+from fastapi import HTTPException, Request
 from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 import pytest
 from pytest_mock import MockerFixture
 from regtech_api_commons.api.exception_handlers import (
+    ERROR_NAME,
+    ERROR_DETAIL,
     regtech_http_exception_handler,
     request_validation_error_handler,
+    http_exception_handler,
+    general_exception_handler,
     log as exception_logger,
 )
 from regtech_api_commons.api.exceptions import RegTechHttpException
@@ -26,8 +31,8 @@ async def test_regtech_http_exception_handler(mocker: MockerFixture, mock_reques
     response = await regtech_http_exception_handler(mock_request, e)
     assert response.status_code == e.status_code
     content = json.loads(response.body)
-    assert content["error_name"] == e.name
-    assert content["error_detail"] == e.detail
+    assert content[ERROR_NAME] == e.name
+    assert content[ERROR_DETAIL] == e.detail
     assert isinstance(content["error_detail"], str)
     error_log_spy.assert_called_with(e, exc_info=True, stack_info=True)
 
@@ -37,7 +42,7 @@ async def test_regtech_http_exception_handler_nested_detail(mock_request: Reques
     response = await regtech_http_exception_handler(mock_request, e)
     assert response.status_code == e.status_code
     content = json.loads(response.body)
-    assert isinstance(content["error_detail"], Dict)
+    assert isinstance(content[ERROR_DETAIL], Dict)
 
 
 async def test_request_validation_error_handler(mock_request: Request):
@@ -46,5 +51,26 @@ async def test_request_validation_error_handler(mock_request: Request):
     response = await request_validation_error_handler(mock_request, rve)
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     content = json.loads(response.body)
-    assert content["error_name"] == "Request Validation Failure"
-    assert content["error_detail"] == errors
+    assert content[ERROR_NAME] == "Request Validation Failure"
+    assert content[ERROR_DETAIL] == errors
+
+async def test_general_http_exception_handler(mock_request: Request):
+    e = HTTPException(HTTPStatus.NOT_FOUND, {"error1": "test1"})
+    response = await http_exception_handler(mock_request, e)
+    content = json.loads(response.body)
+    assert content[ERROR_NAME] == HTTPStatus.NOT_FOUND.phrase
+    assert content[ERROR_DETAIL] == {"error1": "test1"}
+
+async def test_general_http_exception_handler_with_starlette_error(mock_request: Request):
+    e = StarletteHTTPException(HTTPStatus.FORBIDDEN, "test error")
+    response = await http_exception_handler(mock_request, e)
+    content = json.loads(response.body)
+    assert content[ERROR_NAME] == HTTPStatus.FORBIDDEN.phrase
+    assert content[ERROR_DETAIL] == "test error"
+
+async def test_general_exception_handler(mock_request: Request):
+    e = RuntimeError("test runtime error")
+    response = await general_exception_handler(mock_request, e)
+    content = json.loads(response.body)
+    assert content[ERROR_NAME] == HTTPStatus.INTERNAL_SERVER_ERROR.phrase
+    assert content[ERROR_DETAIL] == "server error"
