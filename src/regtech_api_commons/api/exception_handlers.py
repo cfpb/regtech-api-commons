e526c1ee2778c1f96a75d85ac9d22362da67dcb9
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2024-04-18 16:05:07 -0400

    feat: add request validation error handler (#30)
    * feat: add request validation error handler
    
    * feat: ensure json
    
    * feat: add handlers for all catchable exceptions
    
    * fix: linters

--- src/regtech_api_commons/api/exception_handlers.py
+++ src/regtech_api_commons/api/exception_handlers.py
@@ -1,14 +1,47 @@
+from http import HTTPStatus
 import logging
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
 from starlette.requests import Request
+from starlette.exceptions import HTTPException
 
 from regtech_api_commons.api.exceptions import RegTechHttpException
 
 log = logging.getLogger(__name__)
 
+ERROR_NAME = "error_name"
+ERROR_DETAIL = "error_detail"
+
 
 async def regtech_http_exception_handler(request: Request, exception: RegTechHttpException) -> JSONResponse:
     log.error(exception, exc_info=True, stack_info=True)
     return JSONResponse(
-        status_code=exception.status_code, content={"error_name": exception.name, "error_detail": exception.detail}
+        status_code=exception.status_code,
+        content={ERROR_NAME: exception.name, ERROR_DETAIL: jsonable_encoder(exception.detail)},
+    )
+
+
+async def request_validation_error_handler(request: Request, exception: RequestValidationError) -> JSONResponse:
+    log.warn(exception, exc_info=True, stack_info=True)
+    return JSONResponse(
+        status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+        content={ERROR_NAME: "Request Validation Failure", ERROR_DETAIL: jsonable_encoder(exception.errors())},
+    )
+
+
+async def http_exception_handler(request: Request, exception: HTTPException) -> JSONResponse:
+    log.error(exception, exc_info=True, stack_info=True)
+    status = HTTPStatus(exception.status_code)
+    return JSONResponse(
+        status_code=exception.status_code,
+        content={ERROR_NAME: status.phrase, ERROR_DETAIL: jsonable_encoder(exception.detail)},
+    )
+
+
+async def general_exception_handler(request: Request, exception: Exception) -> JSONResponse:
+    log.error(exception, exc_info=True, stack_info=True)
+    return JSONResponse(
+        status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+        content={ERROR_NAME: HTTPStatus.INTERNAL_SERVER_ERROR.phrase, ERROR_DETAIL: "server error"},
     )
