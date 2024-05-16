a8ebaae979447f60887954d7e3fbf51f01f88cee
jmcguffee <justin.mcguffee@cfpb.gov>
2024-05-16 16:14:47 -0400

    Updated associate lei to check if valid and added tests

--- src/regtech_api_commons/oauth2/oauth2_admin.py
+++ src/regtech_api_commons/oauth2/oauth2_admin.py
@@ -10,7 +10,7 @@ from keycloak import KeycloakAdmin, KeycloakOpenIDConnection, exceptions as kce
 
 from regtech_api_commons.models.auth import RegTechUser
 
-from regtech_api_commons.oauth2.config import KeycloakSettings
+from regtech_api_commons.oauth2.config import KeycloakSettings, regex_configs
 
 log = logging.getLogger(__name__)
 
@@ -84,15 +84,13 @@ class OAuth2Admin:
             raise HTTPException(status_code=e.response_code, detail="Failed to associate user to group")
 
     def associate_to_lei(self, user_id: str, lei: str) -> None:
-        group = self.get_group(lei)
-        group_id = group["id"] if group else self._admin.create_group({"name": lei})
-        if group_id:
-            self.associate_to_group(user_id, group_id)
+        if regex_configs.lei.regex.match(lei):
+            group = self.get_group(lei)
+            group_id = group["id"] if group else self._admin.create_group({"name": lei})
+            if group_id:
+                self.associate_to_group(user_id, group_id)
         else:
-            raise HTTPException(
-                status_code=HTTPStatus.BAD_REQUEST,
-                detail="No institution found for given LEI",
-            )
+            raise ValueError(f"Invalid lei {lei}. {regex_configs.lei.error_text}")
 
     def associate_to_leis(self, user_id: str, leis: Set[str]):
         for lei in leis:
